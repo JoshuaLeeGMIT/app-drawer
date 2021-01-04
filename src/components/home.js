70ce3e2f7e80cc7c3fbca5c9e88fbb33eea4381a
Joshua Lee <lee.son.wai@gmail.com>
2021-01-04 20:15:53 +0000

    home: Port to a Jumbotron

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,9 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import React from 'react';
+import Row from 'react-bootstrap/Row';
 
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,7 +12,25 @@ export class Home extends React.Component {
   render() {
     return (
       <div class="App">
-        <h1>Hello, world!</h1>
+        <Jumbotron>
+          <h1>
+            AppDrawer
+          </h1>
+          <h5>
+            Manage your apps the smart way.
+          </h5>
+          <p>
+            <Button variant="outline-primary">
+              Learn more
+            </Button>
+          </p>
+        </Jumbotron>
+        <Container>
+          <Row>
+            <Col>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
