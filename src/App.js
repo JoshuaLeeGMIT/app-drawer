5060c6421956c4757d675229103f210ec413f425
Joshua Lee <lee.son.wai@gmail.com>
2021-01-04 21:13:02 +0000

    app: Add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import {Home} from './components/home';
 
 import './App.css';
@@ -9,12 +11,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
   render() {
     return (
+      <BrowserRouter>
         <div className="App">
           <Navbar bg="light">
             <Navbar.Brand>AppDrawer</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
           </Navbar>
-          <Home></Home>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+          </Switch>
         </div>
+      </BrowserRouter>
     );
   }
 }
